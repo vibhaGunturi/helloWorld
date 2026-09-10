c8b675f2f0f262928f471be9fb5c3742cd3afd94
Vibha Gunturi <vibha.gunturi@gmail.com>
2026-09-10 15:01:22 -0400

    Added my first HTML page with hello world message.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World from Vibha Gunturi! This is my first HTML page</h1>"
+    return "<h1>Hello World from Vibha Gunturi! This is my first code change</h1>"
 
 
 if __name__ == "__main__":
