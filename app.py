5c2c29659fd15773dd1958b6cb6b18bb68500eb2
Vibha Gunturi <vibha.gunturi@gmail.com>
2026-09-10 15:14:53 -0400

    Modified return message

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def hello_world():
+    return "Hello World from Vibha Gunturi! This is my first HTML page."
+
+@app.route('/hello')
 def hello():
-    return "<h1>Hello World from Vibha Gunturi! This is my first code change</h1>"
+    return render_template('hello.html')
 
 
 if __name__ == "__main__":
