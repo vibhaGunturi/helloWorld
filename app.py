ae6f830e4f7266b3aa335eeda7615b03acbd651c
Vibha Gunturi <vibha.gunturi@gmail.com>
2026-09-24 19:29:20 -0400

    HTML page that describes me.

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def hello_world():
 def hello():
     return render_template('hello.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
